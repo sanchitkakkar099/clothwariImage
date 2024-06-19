2ca8be956dc4e96fb109dac18a3063526172c8db
Dheeraj Kumar <{dheerajpanapurlanga@gmail.com}>
2024-06-19 11:35:42 +0530

    mock change

--- src/components/National.js
+++ src/components/National.js
@@ -3,7 +3,7 @@ import { Controller, useFieldArray, useForm } from 'react-hook-form';
 import Select from "react-select";  
 import { CloudLightning, Plus, X } from 'react-feather';
 import { Button, Card, CardBody, CardFooter, CardHeader, Col, Form, Input, Label, Row } from 'reactstrap';
-import imagepath2 from '../images/MicrosoftTeams-image (15).png'
+import imagepath2 from '../images/Domestic.png'
 import logo from '../images/logoww (1).jpg'
 import { setPageStyle, removePageStyle } from '../utils/customPageSize';
 import { useReactToPrint } from "react-to-print";
